b56a4266a6fd910ff0a5dd2e223cc82b315018ee
0jenn0 <jenn0.6n@gmail.com>
2023-12-13 13:29:13 +0900

    Feat: 코치 이름 검증하는 클래스 구현

---
+++ src/Validator/CoachNameValidator.js
@@ -0,0 +1,20 @@
+const CoachNameValidator = {
+  checkNameLength(name) {
+    if (name.length < 2 || name.length > 4) {
+      throw new Error("코치 이름은 2글자 이상 4글자 이하입니다.");
+    }
+  },
+
+  checkIsEmpty(name) {
+    if (name === "") {
+      throw new Error("코치 이름은 공백일 수 없습니다.");
+    }
+  },
+
+  check(name) {
+    this.checkIsEmpty(name);
+    this.checkNameLength(name);
+  },
+};
+
+export default CoachNameValidator;
