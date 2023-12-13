45bf9e5463939f021889ff3d63f8a3066f596fc3
0jenn0 <jenn0.6n@gmail.com>
2023-12-13 15:58:27 +0900

    Feat: 카테고리 최대 2번까지만 중복가능하도록 5번 무작위로뽑는 함수 구현

--- src/Model/Menu.js
+++ src/Model/Menu.js
@@ -1,13 +1,38 @@
-import MENU from "../Constant/Menu";
+const MENU = require("../Constant/Menu.js");
 
-export default class Menu {
-  #menu = MENU;
+class Menu {
+  static menu = MENU;
 
   static isIncluded(menu) {
-    return Object.values(this.#menu).some((menus) => menus.includes(menu));
+    return Object.values(this.menu).some((menus) => menus.includes(menu));
   }
 
   static isIncludedIn(menus) {
     return menus.every((menu) => this.isIncluded(menu));
   }
+
+  static getRandomCategory() {
+    const categories = Object.keys(this.menu);
+    const category = categories[Randoms.pickNumberInRange(1, 5) + 1];
+
+    return category;
+  }
+
+  static pickCategoriesWithLimit(limit = 2, picks = 5) {
+    const pickedCategories = {};
+    const result = [];
+
+    while (result.length < picks) {
+      const category = this.getRandomCategory();
+      pickedCategories[category] = (pickedCategories[category] || 0) + 1;
+
+      if (pickedCategories[category] <= limit) {
+        result.push(category);
+      }
+    }
+
+    return result;
+  }
 }
+
+module.exports = Menu;
