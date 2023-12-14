26c9bb6375fc91699407e407211833da30111b95
0jenn0 <jenn0.6n@gmail.com>
2023-12-15 07:44:43 +0900

    Feat: Coach

--- src/Model/Coach.js
+++ src/Model/Coach.js
@@ -11,32 +11,51 @@ class Coach {
     this.#restrictedMenus = restrictedMenus;
   }
 
+  //   recommendMenus(categories) {
+  //     let recommendedMenus = [];
+
+  //     categories.forEach((category) => {
+  //       let categoryMenus = Menu.menu[category];
+
+  //       while (recommendedMenus.length < 5) {
+  //         const array = Array.from({ length: categoryMenus.length }, (_, i) => i);
+
+  //         let aa = Random.shuffle(array);
+
+  //         let randomIndex = aa[0];
+
+  //         let menu = categoryMenus[randomIndex - 1];
+
+  //         if (
+  //           !recommendedMenus.includes(menu) &&
+  //           !this.#restrictedMenus.includes(menu)
+  //         ) {
+  //           recommendedMenus.push(menu);
+  //           break;
+  //         }
+  //       }
+  //     });
+
+  //     return recommendedMenus;
+  //   }
   recommendMenus(categories) {
-    let recommendedMenus = [];
+    return categories
+      .map((category) => this.pickRandomMenu(category))
+      .filter(Boolean);
+  }
 
-    categories.forEach((category) => {
-      let categoryMenus = Menu.menu[category];
+  pickRandomMenu(category) {
+    const categoryMenus = Menu.menu[category];
+    const shuffledIndexes = Random.shuffle(
+      Array.from({ length: categoryMenus.length }, (_, i) => i)
+    );
 
-      while (recommendedMenus.length < 5) {
-        const array = Array.from({ length: categoryMenus.length }, (_, i) => i);
-
-        let aa = Random.shuffle(array);
-
-        let randomIndex = aa[0];
-
-        let menu = categoryMenus[randomIndex - 1];
-
-        if (
-          !recommendedMenus.includes(menu) &&
-          !this.#restrictedMenus.includes(menu)
-        ) {
-          recommendedMenus.push(menu);
-          break;
-        }
+    for (const index of shuffledIndexes) {
+      const menu = categoryMenus[index - 1];
+      if (!this.#restrictedMenus.includes(menu)) {
+        return menu;
       }
-    });
-
-    return recommendedMenus;
+    }
   }
 
   getName() {
