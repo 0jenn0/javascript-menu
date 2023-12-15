6081313d9f3628533a6aa39485c6a03cb5b57adc
0jenn0 <jenn0.6n@gmail.com>
2023-12-15 09:57:48 +0900

    Test 통과

--- src/Controller/AppController.js
+++ src/Controller/AppController.js
@@ -3,15 +3,13 @@ import CoachValidator from '../Validator/CoachValidator.js';
 import CommonValidator from '../Validator/CommonValidator.js';
 import MenuValidator from '../Validator/MenuValidator.js';
 import { InputView, OutputView } from '../View/index.js';
+import { Console } from '@woowacourse/mission-utils';
 
 export default class AppController {
   async start() {
-    // 시작 메시지 출력
-    // 입력 받기 (prompt)
-    // this.#setupController.initialize();
-    // 서비스 로직
-    // 출력
     OutputView.printStartMessage();
+
+    // 코치 이름 입력받기
     const coachNames = await InputView.promptCoachNames();
     CommonValidator.check(coachNames);
     CoachValidator.check(coachNames);
@@ -27,6 +25,22 @@ export default class AppController {
 
     // 랜덤 카테고리뽑기
     const categories = Menu.pickRandomCategories();
-    console.log('categories', categories);
+
+    // 메뉴 추천
+    const recommandResult = [];
+    coachesWithRestrictedMenus.forEach(({ name, restrictedMenus }) => {
+      const recommandedMenus = Menu.recommendMenu(categories, restrictedMenus);
+      recommandResult.push({ name, recommandedMenus });
+    });
+
+    // 결과 출력
+    Console.print('메뉴 추천 결과입니다.');
+    Console.print('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
+    Console.print(`[ 카테고리 | ${categories.join(' | ')} ]`);
+    recommandResult.forEach(({ name, recommandedMenus }) => {
+      Console.print(`[ ${name} | ${recommandedMenus.join(' | ')} ]`);
+    });
+
+    Console.print(`\n추천을 완료했습니다.`);
   }
 }
--- src/Models/Menu.js
+++ src/Models/Menu.js
@@ -24,7 +24,7 @@ export default class Menu {
     const result = [];
 
     while (result.length < 5) {
-      const number = Random.pickNumberInRange(0, 4);
+      const number = Random.pickNumberInRange(1, 5);
       counts[number] = (counts[number] || 0) + 1;
 
       if (counts[number] <= 2) {
@@ -36,6 +36,33 @@ export default class Menu {
   }
 
   static pickRandomCategories() {
-    return Menu.pickRandomIndexes().map((index) => Menu.getCategories()[index]);
+    return Menu.pickRandomIndexes().map((index) => Menu.getCategories()[index - 1]);
+  }
+
+  static getRandomMenu(category) {
+    const menuNums = Menu.#menu[category].length;
+    const array = Array.from({ length: menuNums }, (_, index) => index + 1);
+    const randomMenuIndex = Random.shuffle(array)[0];
+
+    return Menu.#menu[category][randomMenuIndex - 1];
+  }
+
+  static recommendMenu(categories, restrictedMenus) {
+    return categories.map((category) =>
+      Menu.getRecommendedMenuForCategory(category, restrictedMenus),
+    );
+  }
+
+  static getRecommendedMenuForCategory(category, restrictedMenus) {
+    const MAX_ATTEMPTS = 10;
+    let attempts = 0;
+
+    while (attempts < MAX_ATTEMPTS) {
+      const recommendedMenu = Menu.getRandomMenu(category);
+      if (!restrictedMenus.includes(recommendedMenu)) {
+        return recommendedMenu;
+      }
+      attempts++;
+    }
   }
 }
